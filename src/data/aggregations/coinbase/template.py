decd5a10819a6435e02923869efccbcfed7c03c9
andrewlee240-simplee <andrew.lee@simplee.app>
2020-11-11 04:40:24 -0500

    add more templates

--- src/data/aggregations/coinbase/template.py
+++ src/data/aggregations/coinbase/template.py
@@ -5,7 +5,7 @@ from coinbase.wallet.client import Client
 import datetime
 import pandas as pd
 import logging
-
+from datetime import datetime, date
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
@@ -19,6 +19,21 @@ API_SECRET = os.getenv('COINBASE_SECRET')
 client = Client(API_KEY, API_SECRET)
 accounts = client.get_accounts()
 
+# Column Names for DataFrame
+CN_DF = {
+    'amount' : 'amount',
+    'usd' : 'usd',
+    'rate' : 'rate(cypto/usd)',
+    'date' : 'date',
+    'date_created' : 'created_at',
+    'total_value' : 'total_value',
+    'total_coin' : 'total_coin',
+    'net_change' : 'net_change'
+}
+
+# We should define a class for each CryptoCurrency
+# After defining the class we should create a transaction dataframe
+# Once we have a clean spreadsheet we can see how each transaciton affects our netvalue
 
 def aggregate_transactions(ignore=None):
     # Get Crypto Transactions
@@ -49,12 +64,15 @@ def aggregate_transactions(ignore=None):
             crypt_balance += float(transaction['balance']['amount'])
 
         price_usd = client.get_spot_price(currency_pair = str(crypto) + '-USD')
-        total_balance = crypt_balance * float(price_usd['amount'])
+        price_usd_amount = float(price_usd['amount'])
+        total_balance = crypt_balance * price_usd_amount
 
         cryptos[crypto]['balances'] = {
                                         'usd_balance' : usd_balance,
                                         'net_return' : total_balance - usd_balance,
-                                        'total_balance' : total_balance
+                                        'total_balance' : total_balance,
+                                        'crypto_price' : price_usd_amount,
+                                        'crypt_balance' : crypt_balance
                                     }
         
 
@@ -62,50 +80,75 @@ def aggregate_transactions(ignore=None):
 
     net_return = 0
     total_balance = 0
+    crypto_balance = {}
+
     for crypto in cryptos:
         print(crypto)
         print("\tNet Return $", cryptos[crypto]['balances']['net_return'])
         net_return += cryptos[crypto]['balances']['net_return']
         total_balance += cryptos[crypto]['balances']['total_balance']
-
+        crypto_balance[crypto] = {
+            'total_balance' : cryptos[crypto]['balances']['total_balance'],
+            'net_return' : cryptos[crypto]['balances']['net_return'],
+            'dollar_amount' : cryptos[crypto]['balances']['crypto_price'] * (cryptos[crypto]['balances']['total_balance']),
+            'crypto_price' : cryptos[crypto]['balances']['crypto_price'],
+        }
     results = {
         'total_return' : net_return,
         'total_balance' : total_balance,
         'data' : cryptos
     }
 
-    COLUMN_VALUES_DF = {
-        'amount' : 'amount',
-        'usd' : 'usd',
-        'rate' : 'rate(cypto/usd)',
-        'date' : 'date',
-        'date_created' : 'created_at'
-    }
     crypto_value = {}
-    for x in results['data']:
-        crypto_value[(x , COLUMN_VALUES_DF['amount'])] = []
-        crypto_value[(x , COLUMN_VALUES_DF['usd'])] = []
-        crypto_value[(x , COLUMN_VALUES_DF['rate'])] = []
-        crypto_value[(x , COLUMN_VALUES_DF['date_created'])] = []
-        for trans in results['data'][x]['transactions']:
-            crypto_value[(x , COLUMN_VALUES_DF['amount'])].append(trans['balance']['amount'])
-            crypto_value[(x , COLUMN_VALUES_DF['usd'])].append(trans['native_balance']['amount'])
-            crypto_value[(x , COLUMN_VALUES_DF['rate'])].append(float(trans['balance']['amount']) / float(trans['native_balance']['amount']))
-            crypto_value[(x , COLUMN_VALUES_DF['date_created'])].append(trans[COLUMN_VALUES_DF['date_created']])
+    for crypto in results['data']:
+
+        crypto_value[(crypto , CN_DF['amount'])] = []
+        crypto_value[(crypto , CN_DF['usd'])] = []
+        crypto_value[(crypto , CN_DF['rate'])] = []
+        crypto_value[(crypto , CN_DF['total_value'])] = []
+        crypto_value[(crypto , CN_DF['total_coin'])] = []
+        crypto_value[(crypto , CN_DF['net_change'])] = []
+        crypto_value[(crypto , CN_DF['date_created'])] = []
+        total_value = results['data'][crypto]['balances']['total_balance'] 
+        total_coin = results['data'][crypto]['balances']['crypt_balance']
+        total_spent = results['data'][crypto]['balances']['total_balance']
+        # print(results['data'][crypto]['balances']['total_balance'])
+        for trans in results['data'][crypto]['transactions']:
+            
+            # Date Control Filter
+            # if 
+            total_spent = (results['data'][crypto]['balances']['crypto_price'] - float(trans['native_balance']['amount']) / float(trans['balance']['amount'])) / results['data'][crypto]['balances']['crypto_price'] * 100
+            crypto_value[(crypto , CN_DF['amount'])].append(trans['balance']['amount'])
+            crypto_value[(crypto , CN_DF['usd'])].append(trans['native_balance']['amount'])
+            crypto_value[(crypto , CN_DF['rate'])].append(float(trans['native_balance']['amount']) / float(trans['balance']['amount']))
+            crypto_value[(crypto , CN_DF['date_created'])].append(datetime.strptime(trans[CN_DF['date_created']], '%Y-%m-%dT%XZ'))
+            crypto_value[(crypto , CN_DF['total_value'])].append(total_value)
+            crypto_value[(crypto , CN_DF['total_coin'])].append(total_coin)
+            crypto_value[(crypto , CN_DF['net_change'])].append(total_spent)
+            total_coin -= float(trans['balance']['amount'])
+            total_value = total_coin * (float(trans['native_balance']['amount']) / float(trans['balance']['amount']))
             # crypto_value[(x , 'rate')].append(float(trans['balance']['amount']) / float(trans['native_balance']['amount']))
+        
     df = pd.DataFrame.from_dict(crypto_value, orient='index')
     
     df = df.transpose()
     crypto_index = (crypto_value.keys())
     index = pd.MultiIndex.from_tuples(crypto_index)
-    print(index)
+
     df = pd.DataFrame(df, columns=index)
-    print("---")
-    # df = pd.DataFrame(df, columns=index)
-    print(df)
+
+    # print(df['total_coin'])
+    # print(df[['amount']])
+    print(df.index)
+    print(df.columns)
+    # print(df.xs('created_at', axis=1, level=1, drop_level=False))
+    filter = df.xs('created_at', axis=1, level=1, drop_level=False)
     print("Total Return" , net_return)
     print("Total Balance" , total_balance)
-    
+    # print(df.loc[datetime.date(year=2020,month=1,day=1):datetime.now()])
+    print(filter)
+    for x in crypto_balance:
+        print(x , crypto_balance[x])
     return results
 
 IGNORE = ['USD']
